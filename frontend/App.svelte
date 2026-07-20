a7ae5be3a639695f68ad27b2b4e0c2e0295017f2
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2026-07-20 21:48:52 +0200

    Svelte cell component

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,8 +1,23 @@
 <script>
-  import Counter from './lib/Counter.svelte'
+  import Cell from './lib/Cell.svelte';
 </script>
 
 <section>
- <Counter />
-
+  <div class="notebook">
+  <Cell />
+  <Cell />
+  <Cell />
+  <Cell />
+  </div>
 </section>
+
+<style>
+  .notebook {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 3rem 0rem;
+    gap: 3rem;
+  }
+</style>
---
+++ frontend/lib/Cell.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Monaco from 'svelte-monaco';
+
+    let value = 'print("Hello, World!")';
+</script>
+
+<div class="cell">
+<Monaco
+options={{ language: 'python', automaticLayout: true }}
+	    theme="vs-dark"
+		on:ready={(event) => console.log(event.detail)}
+		bind:value
+/>
+</div>
+ 
+<style>
+    .cell {
+        height: 10rem;
+    } 
+</style>
